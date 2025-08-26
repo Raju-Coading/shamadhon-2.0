2eb03071652e8015d43fac736135e5d0d4334e75
Raju-Coading <161103049+Raju-Coading@users.noreply.github.com>
2025-08-26 11:32:29 +0530

    Create day_1_task.js

---
+++ day_1_task/day_1_task.js
@@ -0,0 +1,14 @@
+// Student object
+const student = {
+  name: "Rahul Sharma",
+  age: 20,
+  course: "Computer Science",
+  rollNo: "CS101"
+};
+
+// Printing student details
+console.log("🎓 Student Details");
+console.log("Name   :", student.name);
+console.log("Age    :", student.age);
+console.log("Course :", student.course);
+console.log("Roll No:", student.rollNo);
