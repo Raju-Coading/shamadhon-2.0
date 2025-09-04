cb0dc3a05f189c3a6d26960690e2dcbf12ebba9b
Raju-Coading <161103049+Raju-Coading@users.noreply.github.com>
2025-09-05 00:18:40 +0530

    Create Footer.jsx

---
+++ day_21_task/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-blue-600 text-white text-center p-4 mt-6">
+      <p>© 2025 My Portfolio | Built with React & Node.js</p>
+    </footer>
+  );
+}
+
+export default Footer;
